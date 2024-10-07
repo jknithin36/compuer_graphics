1f8f9e80ee055e20e408bf2c07bc3ad5e33d9684
J.K. Nithin Kumar <89259785+jknithin36@users.noreply.github.com>
2024-10-07 13:07:14 -0400

    image_intial

--- final_code/primitives/Sphere.cpp
+++ final_code/primitives/Sphere.cpp
@@ -1,75 +1,160 @@
+// #include "Sphere.h"
+// #include "../Imageio/Imageio.h"
+// #include <math.h>
+
+// Intersect_Cond Sphere::intersection_check(const M3DVector3f start, const M3DVector3f dir, float & distance, M3DVector3f intersection_p)
+// {
+// M3DVector3f ray_to_center;
+// m3dSubtractVectors3(ray_to_center,_pos,start);
+// float ray_center_length = m3dDotProduct(ray_to_center, ray_to_center);
+
+// float closest_point = m3dDotProduct( ray_to_center, dir);
+// if (closest_point < 0)
+// {
+// return _k_miss;
+// }
+
+// float halfCord2 = (_rad * _rad) - (ray_center_length - (closest_point * closest_point));  
+// if(halfCord2 < 0)
+// {
+// return _k_miss;
+// }
+
+// Intersect_Cond type;
+// M3DVector3f tmp;
+// m3dSubtractVectors3(tmp,start,_pos);
+// float length = m3dDotProduct(tmp,tmp);
+// if (length < _rad2)
+// {
+// type = _k_inside;
+// distance = closest_point + sqrt(halfCord2);
+// }else
+// {
+// type = _k_hit;
+// distance = closest_point - sqrt(halfCord2);
+// }
+
+// M3DVector3f tmp_v;
+// m3dCopyVector3(tmp_v, dir);
+// m3dScaleVector3(tmp_v,distance);
+// m3dAddVectors3(intersection_p,start,tmp_v);
+
+// return type;
+// }
+
+// void Sphere::shade(M3DVector3f view,M3DVector3f intersect_p,const Light & sp_light, M3DVector3f am_light, M3DVector3f color, bool shadow)
+// {
+// M3DVector3f light_spot, light_illumination;
+// M3DVector3f l;
+// M3DVector3f m;
+// M3DVector3f n;
+// sp_light.get_light(light_spot, light_illumination);
+// m3dSubtractVectors3(l, intersect_p, _pos);
+// m3dNormalizeVector(l);
+// m3dSubtractVectors3(m, light_spot, intersect_p);
+// m3dNormalizeVector(m);
+// float scatter = m3dDotProduct(m, l);
+// m3dCopyVector3(n, l);
+// m3dScaleVector3(n, 2 * scatter);
+// m3dSubtractVectors3(n, m, n);
+// float spec = m3dDotProduct(n, view);
+// for (int i = 0; i < 3; i++)
+// {
+// color[i] = _ka * _color[i] * am_light[i];
+
+// if (scatter > 0)
+// {
+// color[i] += _kd * _color[i] * light_illumination[i] * scatter;
+// }
+// if (spec > 0)
+// {
+// color[i] += _ks * light_illumination[i] * pow(spec, 100);
+// }
+// }
+
+// }
+
 #include "Sphere.h"
 #include "../Imageio/Imageio.h"
-#include <math.h>
+#include <cmath> // Use <cmath> instead of <math.h> for C++
 
-Intersect_Cond Sphere::intersection_check(const M3DVector3f start, const M3DVector3f dir, float & distance, M3DVector3f intersection_p)
-{
-M3DVector3f ray_to_center;
-m3dSubtractVectors3(ray_to_center,_pos,start);
-float ray_center_length = m3dDotProduct(ray_to_center, ray_to_center);
+Intersect_Cond Sphere::intersection_check(const M3DVector3f start, const M3DVector3f dir, float &distance, M3DVector3f intersection_p) {
+    M3DVector3f ray_to_center;
+    m3dSubtractVectors3(ray_to_center, _pos, start);
+    
+    // Calculate the squared length from the ray start to the sphere center
+    float ray_center_length_squared = m3dDotProduct(ray_to_center, ray_to_center);
 
-float closest_point = m3dDotProduct( ray_to_center, dir);
-if (closest_point < 0)
-{
-return _k_miss;
+    // Calculate the closest point on the ray to the center of the sphere
+    float closest_point = m3dDotProduct(ray_to_center, dir);
+    if (closest_point < 0) {
+        return _k_miss; // The ray misses the sphere
+    }
+
+    // Calculate half the chord squared
+    float halfChordSquared = (_rad * _rad) - (ray_center_length_squared - (closest_point * closest_point));
+    if (halfChordSquared < 0) {
+        return _k_miss; // The ray misses the sphere
+    }
+
+    // Determine intersection type and calculate distance to the intersection point
+    Intersect_Cond type;
+    M3DVector3f tmp;
+    m3dSubtractVectors3(tmp, start, _pos);
+    float length_squared = m3dDotProduct(tmp, tmp);
+    
+    if (length_squared < _rad2) {
+        type = _k_inside;
+        distance = closest_point + sqrt(halfChordSquared);
+    } else {
+        type = _k_hit;
+        distance = closest_point - sqrt(halfChordSquared);
+    }
+
+    // Calculate the intersection point
+    M3DVector3f tmp_v;
+    m3dCopyVector3(tmp_v, dir);
+    m3dScaleVector3(tmp_v, distance);
+    m3dAddVectors3(intersection_p, start, tmp_v);
+
+    return type; // Return the intersection condition
 }
 
-float halfCord2 = (_rad * _rad) - (ray_center_length - (closest_point * closest_point));  
-if(halfCord2 < 0)
-{
-return _k_miss;
-}
+void Sphere::shade(M3DVector3f view, M3DVector3f intersect_p, const Light &sp_light, M3DVector3f am_light, M3DVector3f color, bool shadow) {
+    M3DVector3f light_spot, light_illumination;
+    sp_light.get_light(light_spot, light_illumination);
+    
+    // Calculate the vector from the sphere center to the intersection point
+    M3DVector3f l;
+    m3dSubtractVectors3(l, intersect_p, _pos);
+    m3dNormalizeVector(l);
+    
+    // Calculate the vector from the intersection point to the light source
+    M3DVector3f m;
+    m3dSubtractVectors3(m, light_spot, intersect_p);
+    m3dNormalizeVector(m);
 
-Intersect_Cond type;
-M3DVector3f tmp;
-m3dSubtractVectors3(tmp,start,_pos);
-float length = m3dDotProduct(tmp,tmp);
-if (length < _rad2)
-{
-type = _k_inside;
-distance = closest_point + sqrt(halfCord2);
-}else
-{
-type = _k_hit;
-distance = closest_point - sqrt(halfCord2);
-}
+    // Calculate the diffuse component
+    float scatter = m3dDotProduct(m, l);
+    
+    // Calculate reflection vector for specular component
+    M3DVector3f n;
+    m3dCopyVector3(n, l);
+    m3dScaleVector3(n, 2 * scatter);
+    m3dSubtractVectors3(n, m, n);
+    
+    // Calculate the specular component
+    float spec = m3dDotProduct(n, view);
+    
+    // Calculate the final color based on ambient, diffuse, and specular contributions
+    for (int i = 0; i < 3; i++) {
+        color[i] = _ka * _color[i] * am_light[i]; // Ambient component
 
-M3DVector3f tmp_v;
-m3dCopyVector3(tmp_v, dir);
-m3dScaleVector3(tmp_v,distance);
-m3dAddVectors3(intersection_p,start,tmp_v);
-
-return type;
-}
-
-void Sphere::shade(M3DVector3f view,M3DVector3f intersect_p,const Light & sp_light, M3DVector3f am_light, M3DVector3f color, bool shadow)
-{
-M3DVector3f light_spot, light_illumination;
-M3DVector3f l;
-M3DVector3f m;
-M3DVector3f n;
-sp_light.get_light(light_spot, light_illumination);
-m3dSubtractVectors3(l, intersect_p, _pos);
-m3dNormalizeVector(l);
-m3dSubtractVectors3(m, light_spot, intersect_p);
-m3dNormalizeVector(m);
-float scatter = m3dDotProduct(m, l);
-m3dCopyVector3(n, l);
-m3dScaleVector3(n, 2 * scatter);
-m3dSubtractVectors3(n, m, n);
-float spec = m3dDotProduct(n, view);
-for (int i = 0; i < 3; i++)
-{
-color[i] = _ka * _color[i] * am_light[i];
-
-if (scatter > 0)
-{
-color[i] += _kd * _color[i] * light_illumination[i] * scatter;
-}
-if (spec > 0)
-{
-color[i] += _ks * light_illumination[i] * pow(spec, 100);
-}
-}
-
+        if (scatter > 0) {
+            color[i] += _kd * _color[i] * light_illumination[i] * scatter; // Diffuse component
+        }
+        if (spec > 0) {
+            color[i] += _ks * light_illumination[i] * pow(spec, 100); // Specular component
+        }
+    }
 }
--- final_code/primitives/Wall.cpp
+++ final_code/primitives/Wall.cpp
@@ -1,44 +1,87 @@
+// #include "Wall.h"
+// #include <math.h>
+// #include "../Imageio/Imageio.h"
+
+// Intersect_Cond Wall::intersection_check(const M3DVector3f start, const M3DVector3f dir, float &distance, M3DVector3f intersection_p)
+// {
+// 	if (_tr1.intersection_check(start,dir,distance,intersection_p) == _k_hit)
+// 	{
+// 		return _k_hit;
+// 	}else
+// 	{
+// 		return _tr2.intersection_check(start,dir,distance,intersection_p);
+// 	}
+// }
+
+
+// void	Wall::shade(M3DVector3f view, M3DVector3f intersect_p,const Light & sp_light, M3DVector3f am_light, M3DVector3f color,bool shadow)
+// {
+// 	for (int i = 0; i < 3; i++)
+// 	{
+// 		color[i] = _color[i];
+// 	}
+// }
+
+
+// void	Wall::get_reflect_direct(const M3DVector3f direct0,const M3DVector3f intersect_p, M3DVector3f reflect_direct)
+// {
+	
+// }
+
+
+// void Wall::load_texture(std::string file_name)
+// {
+	
+// }
+
+// void Wall::texture_color(M3DVector3f pos, M3DVector3f color)
+// {
+// }
+
+// void	Wall::get_texel(float x, float y, M3DVector3f color)
+// {
+// 	return;
+// }
+
 #include "Wall.h"
-#include <math.h>
+#include <cmath> // Use <cmath> instead of <math.h> for C++
 #include "../Imageio/Imageio.h"
 
-Intersect_Cond Wall::intersection_check(const M3DVector3f start, const M3DVector3f dir, float &distance, M3DVector3f intersection_p)
-{
-	if (_tr1.intersection_check(start,dir,distance,intersection_p) == _k_hit)
-	{
-		return _k_hit;
-	}else
-	{
-		return _tr2.intersection_check(start,dir,distance,intersection_p);
-	}
+// Check if the ray intersects with either triangle of the wall
+Intersect_Cond Wall::intersection_check(const M3DVector3f start, const M3DVector3f dir, float &distance, M3DVector3f intersection_p) {
+    // Check intersection with the first triangle
+    if (_tr1.intersection_check(start, dir, distance, intersection_p) == _k_hit) {
+        return _k_hit; // Hit if the first triangle is intersected
+    } else {
+        // Check intersection with the second triangle
+        return _tr2.intersection_check(start, dir, distance, intersection_p);
+    }
 }
 
-
-void	Wall::shade(M3DVector3f view, M3DVector3f intersect_p,const Light & sp_light, M3DVector3f am_light, M3DVector3f color,bool shadow)
-{
-	for (int i = 0; i < 3; i++)
-	{
-		color[i] = _color[i];
-	}
+// Shade the wall with its base color (currently no shading calculations)
+void Wall::shade(M3DVector3f view, M3DVector3f intersect_p, const Light &sp_light, M3DVector3f am_light, M3DVector3f color, bool shadow) {
+    for (int i = 0; i < 3; i++) {
+        color[i] = _color[i]; // Set color based on the wall's color
+    }
 }
 
-
-void	Wall::get_reflect_direct(const M3DVector3f direct0,const M3DVector3f intersect_p, M3DVector3f reflect_direct)
-{
-	
+// Placeholder for calculating reflected direction (not implemented)
+void Wall::get_reflect_direct(const M3DVector3f direct0, const M3DVector3f intersect_p, M3DVector3f reflect_direct) {
+    // Functionality not implemented yet
 }
 
-
-void Wall::load_texture(std::string file_name)
-{
-	
+// Load a texture from a file (not implemented)
+void Wall::load_texture(std::string file_name) {
+    // Functionality not implemented yet
 }
 
-void Wall::texture_color(M3DVector3f pos, M3DVector3f color)
-{
+// Get the color from the texture at a specific position (not implemented)
+void Wall::texture_color(M3DVector3f pos, M3DVector3f color) {
+    // Functionality not implemented yet
 }
 
-void	Wall::get_texel(float x, float y, M3DVector3f color)
-{
-	return;
+// Get the texel color at the given texture coordinates (not implemented)
+void Wall::get_texel(float x, float y, M3DVector3f color) {
+    // Functionality not implemented yet
+    return;
 }
